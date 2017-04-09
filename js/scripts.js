ebb27ee82bd4a97d2a5b71066180813c28d95fa1
Krzysztof Barański <Krzysztof Baraski>
2017-04-10 00:09:00 +0200

    --all

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+
+function drawTree(p1) {
+
+  for ( var i = 0 ; i < p1 ; i++) {
+    var star = '';
+      for( var j = 0 ; j < p1 ; j++) {
+        star += '*';
+            console.log(star);
+      }
+  }
+}
+var x = drawTree(6);
